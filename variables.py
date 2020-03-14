f1d9f995545c8debdac3cb51245ba590ff4fbab0
Yuyun Wu <1248276514@qq.com>
2020-03-14 14:38:26 +0800

    p5

---
+++ variables.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 22:19:34 2020
+
+@author: halowin
+"""
+
+a=527
+b=527527
+b%7==0
+c=b/7
+d=c/11
+e=d/13
+e>a
+
+
+X=True;
+Y=False;
+Z=(X and not Y) or (Y and not X);
+if Z is True:
+    print(1);
+else:
+    print(0);
